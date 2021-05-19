19baf554d550d9047c76293fa771499cc1835525
Kamil Mazur <KamilMaz1357@gmail.com>
2021-05-19 19:24:07 +0200

    Zrobienie zadania 4 do sprawdzenia

--- ZadanieDomowe/src/zadaniedomowe/Zadanie4.java
+++ ZadanieDomowe/src/zadaniedomowe/Zadanie4.java
@@ -5,10 +5,33 @@
  */
 package zadaniedomowe;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Kamil
  */
 public class Zadanie4 {
+     public static void main (String args[]){
+        Scanner s=new Scanner(System.in);
     
+  double a;
+        double b;
+        double suma,roznica,iloczyn,iloraz;
+        
+    
+    
+    a=s.nextDouble();
+    b=s.nextDouble();
+    
+     suma=a+b;
+        roznica=a-b;
+        iloczyn=a*b;
+        iloraz=a/b;
+        
+          System.out.println("suma = "+suma);
+          System.out.println("roznica = "+roznica);
+          System.out.println("iloczyn = "+iloczyn);
+          System.out.println("iloraz = "+iloraz);
+}
 }
